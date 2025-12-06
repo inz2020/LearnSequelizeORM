d1969f9fdac6a69a9b6734227482fa1ef7aa4062
inz2020 <72919182+inz2020@users.noreply.github.com>
2025-12-06 11:11:57 +0100

    Config env variables

--- db/config/config.js
+++ db/config/config.js
@@ -1,8 +1,18 @@
 const path = require('path');
 const fs = require('fs');
 
-const configFile = JSON.parse(
-  fs.readFileSync(path.resolve(__dirname, 'config.json'), 'utf-8')
-);
+// const configFile = JSON.parse(
+//   fs.readFileSync(path.resolve(__dirname, 'config.json'), 'utf-8')
+// );
+ require('dotenv').config();
 
-module.exports = configFile;
+module.exports = {
+  development: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT
+  }
+};
+//module.exports = configFile;
